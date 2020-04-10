0a0aec553fb6301db5cd24ae313cfa3442c0db4f
nullcjm <1029169758@qq.com>
2020-04-10 19:18:37 +0800

    push

---
+++ pages/UserCenter/UserCenter.vue
@@ -0,0 +1,217 @@
+<template>
+	<view class="center">
+		<view class="center_top">
+			<view class="mask"></view>
+		</view>
+		<view class="center_box_bg">
+			<view class="profily">
+				<view class="base">
+					<view class="profily_header">
+
+					</view>
+					<text>昵称</text>
+				</view>
+			</view>
+			<view class="baiban">
+
+			</view>
+			<view class="center_menu">
+				<view class="menu_item" v-for="item in menus">
+					<image :src="item.icon" mode="aspectFill"></image>
+					<text>{{item.name}}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				menus: [{
+						name: '花朵图鉴',
+						icon: '../../static/images/图鉴.png',
+						key: 1,
+					},
+					{
+						name: '个人成就',
+						icon: '../../static/images/成就.png',
+						key: 2,
+					},
+					{
+						name: '月种植数',
+						icon: '../../static/images/种植.png',
+						key: 3,
+					},
+					{
+						name: '帮助中心',
+						icon: '../../static/images/8.png',
+						key: 4,
+					},
+					{
+						name: '意见反馈',
+						icon: '../../static/images/9.png',
+						key: 5,
+					},
+					{
+						name: '关于我们',
+						icon: '../../static/images/10.png',
+						key: 6,
+					}
+
+				]
+			};
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	page {
+		height: 100%;
+	}
+
+	.profily,
+	.profily_header {
+		border-radius: 8px;
+	}
+
+	.center {
+		height: 100%;
+
+		&_top {
+			height: 18%;
+			background: url('../../static/images/user.png') no-repeat 0% 50%;
+			background-size: cover;
+
+			// background: #E6E6E6;
+			.mask {
+				background: rgba(0, 0, 0, .4);
+				height: 100%;
+			}
+		}
+
+		&_box_bg {
+			background: #F9F9F9;
+			position: relative;
+
+			.profily {
+				position: absolute;
+				width: 90%;
+				// border:1px solid #F7F7F7;
+				margin: 0 auto;
+				top: -100upx;
+				left: 5%;
+				background: #FEFEFE;
+				padding: 35upx;
+				box-sizing: border-box;
+				box-shadow: 0px 2px 5px #EDEDED;
+			}
+		}
+	}
+
+	.base {
+		display: flex;
+		align-items: center;
+		border-bottom: 2px solid #F6F6F6;
+		padding-bottom: 35upx;
+		position: relative;
+		.profily_header {
+			height: 120upx;
+			width: 120upx;
+			background-image: url('../../static/images/user.png');
+			background-size: 100%;
+		}
+
+		text {
+			margin-left: 20px;
+			font-size: 30upx;
+		}
+		
+		image{
+			position: absolute;
+			height: 40upx;
+			width: 40upx;
+			right: 0px;
+			top:0px;
+		}
+	}
+
+	.order_status {
+		display: flex;
+		justify-content: space-between;
+		margin-top: 35upx;
+
+		.status {
+			width: 140upx;
+			font-size: 24upx;
+			text-align: center;
+			letter-spacing: .5px;
+			display: flex;
+			flex-direction: column;
+			.icon {
+				width: 50upx;
+				height: 50upx;
+				margin: 0 auto;
+				margin-bottom: 5px;
+				
+			}
+		}
+	}
+
+	.baiban {
+		background: #FEFEFE;
+		height: 200upx;
+	}
+
+	.center_menu {
+		width: 100%;
+		display: inline-block;
+
+		.menu_item {
+			font-size: 28upx;
+			letter-spacing: 1px;
+			padding: 14px 5%;
+			background: #FEFEFE;
+			overflow: hidden;
+			box-sizing: border-box;
+			display: flex;
+			align-items: center;
+			position: relative;
+			border-bottom: 1px solid #EFEFEF;
+
+			&:hover {
+				background: #F6F6F6 !important;
+			}
+
+			&::after {
+				content: '';
+				width: 30upx;
+				height: 30upx;
+				position: absolute;
+				right: 5%;
+				background: url('../../static/images/arrow_right.png') no-repeat;
+				background-size: 100%;
+			}
+
+			text:nth-of-type(1) {
+				margin-left: 10px;
+			}
+
+			image {
+				width: 40upx;
+				height: 40upx;
+			}
+
+			&:nth-of-type(4) {
+				margin-top: 10px;
+			}
+		}
+	}
+</style>
