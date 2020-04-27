0dbd8c7e5e78a9befb4462136681b909efa78d90
nullcjm <1029169758@qq.com>
2020-04-27 15:54:25 +0800

    新增成就页面内容

--- pages/honor/honor.vue
+++ pages/honor/honor.vue
@@ -1,6 +1,19 @@
 <template>
 	<view>
-		个人成就
+		<uni-list>
+			<uni-list-item title="个人成就" :disabled="true" :show-arrow="false"></uni-list-item>
+			<uni-list-item title="第一朵花" note="步数累计超一千步,获得第一朵花" :show-arrow="false" thumb="../../static/images/king_checked.png"></uni-list-item>
+			<uni-list-item title="行万里路" note="近31天步数累计超十万步" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+			<uni-list-item title="其他成就" note="#" :show-arrow="false" thumb="../../static/images/king.png"></uni-list-item>
+		</uni-list>
 	</view>
 </template>
 
